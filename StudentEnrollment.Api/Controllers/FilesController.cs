570c49f6e5e9db7b1473ab77bdae167cf91b81e8
YawDev <94807175+YawDev@users.noreply.github.com>
2022-01-21 22:06:49 -0500

    file-view

--- StudentEnrollment.Api/Controllers/FilesController.cs
+++ StudentEnrollment.Api/Controllers/FilesController.cs
@@ -2,6 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentEnrollment.Api.Utils;
+using StudentEnrollment.API.Controllers;
+using StudentEnrollment.Core;
+using StudentEnrollment.Core.Exceptions;
+using StudentEnrollment.Core.Files;
 
 namespace StudentEnrollment.Api.Controllers
 {
@@ -33,6 +40,77 @@ namespace StudentEnrollment.Api.Controllers
                 try
                 {
                     var result = _messages.Dispatch(query);
+                    return Ok(result);
+                }
+                catch (DomainException ex)
+                {
+                    _logger.LogError(ex.Message);
+                    return Error(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogCritical(ex.Message);
+                    return StatusCode(500);
+                }
+
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// Get file by id
+        /// </summary>
+        /// <response code="400">Bad request</response> 
+        /// <response code="401">Unknown Identity</response>
+        /// <response code="403">Unauthorized</response>
+
+        [HttpGet("student-enrollment/api/filecontent/{id}")]
+        [ProducesResponseType(typeof(Envelope), 201)]
+        [ProducesResponseType(typeof(Envelope), 400)]
+        public IActionResult GetFileById(Guid id)
+        {
+            if (ModelState.IsValid)
+            {
+                GetFileById query = new GetFileById(id);
+                try
+                {
+                    var result = _messages.Dispatch(query);
+                    return Ok(result);
+                }
+                catch (DomainException ex)
+                {
+                    _logger.LogError(ex.Message);
+                    return Error(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogCritical(ex.Message);
+                    return StatusCode(500);
+                }
+
+            }
+            return BadRequest();
+        }
+
+
+        /// <summary>
+        /// Delete File
+        /// </summary>
+        /// <response code="400">Bad request</response> 
+        /// <response code="401">Unknown Identity</response>
+        /// <response code="403">Unauthorized</response>
+
+        [HttpDelete("student-enrollment/api/uploads/delete/{id}")]
+        [ProducesResponseType(typeof(Envelope), 201)]
+        [ProducesResponseType(typeof(Envelope), 400)]
+        public IActionResult DeleteUploadedFile(Guid id)
+        {
+            if (ModelState.IsValid)
+            {
+                DeleteFile command = new DeleteFile(id);
+                try
+                {
+                    var result = _messages.Dispatch(command);
                     return Ok(result);
                 }
                 catch (DomainException ex)
